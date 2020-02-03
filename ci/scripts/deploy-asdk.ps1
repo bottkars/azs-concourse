313c4a794ae63c34c90701147e3036b45a2a2f7c
Karsten Bott <karsten.bott@bottnet.de>
2020-02-03 11:56:58 +0100

    latest connect

--- ci/scripts/deploy-asdk.ps1
+++ ci/scripts/deploy-asdk.ps1
@@ -1,1 +1,5 @@
-Get-Variable
+
+
+$password = $env:ASDK_PASSWORD | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $env:ASDK_USERNAME, $password
+Enter-PSSession -ComputerName $env:ASDK_HOST -Authentication Negotiate -Credential $credential    
