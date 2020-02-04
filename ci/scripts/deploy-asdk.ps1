292afabae390a4ea0ef3739a483ea25e6a5d29f1
Karsten Bott <karsten.bott@bottnet.de>
2020-02-04 12:48:38 +0100

    a first test

--- ci/scripts/deploy-asdk.ps1
+++ ci/scripts/deploy-asdk.ps1
@@ -8,4 +8,7 @@ set-item WSman:\localhost\Client\TrustedHosts -value $env:ASDK_HOST -Force
 Get-Item WSMan:\localhost\Client\TrustedHosts
 
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azurestack\Azurestackadmin", $password
-Enter-PSSession -ComputerName $env:ASDK_HOST -Authentication Negotiate -Credential $credential
+# Enter-PSSession -ComputerName $env:ASDK_HOST -Authentication Negotiate -Credential $credential
+
+Invoke-Command -ComputerName $env:ASDK_HOST -ScriptBlock { Get-ComputerName } -credential $credential
+
