1730b11e3cfb556293e384ff03cfdc4aa6af530e
Karsten Bott <karsten.bott@bottnet.de>
2020-02-04 23:17:43 +0100

    test copy operation

--- ci/scripts/deploy-asdk.ps1
+++ ci/scripts/deploy-asdk.ps1
@@ -10,8 +10,6 @@ Get-Item WSMan:\localhost\Client\TrustedHosts
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azurestack\Azurestackadmin", $password
 $Session = New-PSSession -ComputerName $env:ASDK_HOST -Authentication Negotiate -Credential $credential
 $Session
-Invoke-Command -ComputerName $env:ASDK_HOST -ScriptBlock { Get-ComputerInfo } -credential $credential
-Get-ChildItem -Path ./ -Recurse
+Invoke-Command -ComputerName $env:ASDK_HOST -ScriptBlock { new-item -ItemType Directory -Path e:\1910 -Force } -Credential  $credential 
 write-host "Now copying ASDK"
-Invoke-Command -ComputerName $env:ASDK_HOST -ScriptBlock { new-item -ItemType Directory -Path e:\1910 -Force  } -Credential  $credential 
-Copy-Item ./cloudbuilder/* -Recurse -Destination c:\test -ToSession $Session
+Copy-Item ./cloudbuilder/* -Recurse -Destination e:\1910 -ToSession $Session -PassThru
