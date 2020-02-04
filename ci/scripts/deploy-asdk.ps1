f03b886796b144b785405e654a2017c6eeea261e
Karsten Bott <karsten.bott@bottnet.de>
2020-02-04 12:26:48 +0100

    vecho stuff

--- ci/scripts/deploy-asdk.ps1
+++ ci/scripts/deploy-asdk.ps1
@@ -1,6 +1,8 @@
 $password = $env:ASDK_PASSWORD | ConvertTo-SecureString -AsPlainText -Force
+
+write-host "$($env:ASDK_HOST) $($env:ASDK_PASSWORD)"
 Get-Item WSMan:\localhost\Client\TrustedHosts
-Set-Item WSMan:\localhost\Client\TrustedHosts "$($env:ASDK_HOST)"
+Set-Item WSMan:\localhost\Client\TrustedHosts -value "$($env:ASDK_HOST)"
 Get-Item WSMan:\localhost\Client\TrustedHosts
 
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azurestack\Azurestackadmin", $password
