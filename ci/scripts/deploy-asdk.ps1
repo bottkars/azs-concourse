f5595fabcf26ac0031c9bbafa94e3d662ce2e4a6
Karsten Bott <karsten.bott@bottnet.de>
2020-02-04 12:32:59 +0100

    wsman trusted hosts

--- ci/scripts/deploy-asdk.ps1
+++ ci/scripts/deploy-asdk.ps1
@@ -2,7 +2,9 @@ $password = $env:ASDK_PASSWORD | ConvertTo-SecureString -AsPlainText -Force
 
 get-item  env:ASDK*
 Get-Item WSMan:\localhost\Client\TrustedHosts
-Set-Item WSMan:\localhost\Client\TrustedHosts -value "$($env:ASDK_HOST)"
+
+set-item WSman:\localhost\Client\TrustedHosts -value $ASDK_HOST
+
 Get-Item WSMan:\localhost\Client\TrustedHosts
 
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azurestack\Azurestackadmin", $password
