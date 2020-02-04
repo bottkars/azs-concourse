478da6230b92467ac3a7b7cb40242260c1e677dc
Karsten Bott <karsten.bott@bottnet.de>
2020-02-04 12:17:25 +0100

    latest

--- ci/scripts/deploy-asdk.ps1
+++ ci/scripts/deploy-asdk.ps1
@@ -1,3 +1,4 @@
 $password = $env:ASDK_PASSWORD | ConvertTo-SecureString -AsPlainText -Force
+Get-Item WSMan:\localhost\Client\TrustedHosts
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azurestack\Azurestackadmin", $password
 Enter-PSSession -ComputerName $env:ASDK_HOST -Authentication Negotiate -Credential $credential
