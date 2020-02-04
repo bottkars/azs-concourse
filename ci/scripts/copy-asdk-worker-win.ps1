46ab9b19a06b4e0264ff03d114afba2c48293ef9
Karsten Bott <karsten.bott@bottnet.de>
2020-02-04 15:12:57 +0100

    latest

--- ci/scripts/copy-asdk-worker-win.ps1
+++ ci/scripts/copy-asdk-worker-win.ps1
@@ -6,7 +6,7 @@ $protocol = Split-Path -Qualifier $env:endpoint
 
 
 $target = $env:endpoint.Replace('//','').split(':')[1]
-$env:MC_HOST_TARGET = "$($protocol)//$($env:access_key_id):$($env:secret_access_key)@$($target):9000"
+$env:MC_HOST_TARGET = "$($protocol)//$($env:access_key_id):$($env:secret_access_key)@$($target)"
 # export MC_HOST_myalias=https://Q3AM3UQ867SPQQA43P2F:zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG@play.min.io
 ./mc.exe ls TARGET
 
