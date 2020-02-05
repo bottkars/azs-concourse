f7933f77533388b8cbb8691c3a6d168f49277472
Karsten Bott <karsten.bott@bottnet.de>
2020-02-05 10:03:02 +0100

    expression handling

--- ci/scripts/dehydrate_cloudbuilder.ps1
+++ ci/scripts/dehydrate_cloudbuilder.ps1
@@ -15,15 +15,15 @@ Write-Host ( $result | Out-String )
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azurestack\Azurestackadmin", $password
 $Session = New-PSSession -ComputerName $env:ASDK_HOST -Authentication Negotiate -Credential $credential
 Write-Host ( $Session | Out-String )
-Write-Host "Now dehydrating Cloudbuilder"
+$expression="$($env:ASDK_FILE_DESTINATION)\AzureStackDevelopmentKit.exe /VERYSILENT /SUPPRESSMESGBOXES /DIR=`"$($env:ASDK_FILE_DESTINATION)`""
+Write-Host "Now dehydrating Cloudbuilder using $expression"
 $parameters = @{
     ComputerName = $env:ASDK_HOST
     ScriptBlock  = { 
-        $expression="$($args[0])\AzureStackDevelopmentKit.exe /VERYSILENT /SUPPRESSMESGBOXES /DIR=`"$($args[0])`""
-        write-host $expression
-        invoke-expression -command $expression
+        write-host $args[0]
+        invoke-expression -command $args[0]
     }  
-    ArgumentList = "$env:ASDK_FILE_DESTINATION" 
+    ArgumentList = $expression 
     Session = $Session   
 }
 
