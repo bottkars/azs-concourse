189ee8bbc8778ad45d6a360d44041244b02438f9
Karsten Bott <karsten.bott@bottnet.de>
2020-02-26 08:26:51 +0100

    pre-version of deploy aks

--- ci/scripts/deploy-aks.sh
+++ ci/scripts/deploy-aks.sh
@@ -14,6 +14,36 @@ az login --service-principal \
     --tenant ${AZURE_TENANT_ID}
 set -eux
 az account set --subscription ${AZURE_SUBSCRIPTION_ID}
+TAG=$(cat aks-engine/tag)
+tar xzfv aks-engine/aks-engine-${TAG}-linux-amd64.tar.gz
+export SSL_CERT_FILE=${AZURE_CLI_CA_PATH}
+ ./aks-engine-${TAG}-linux-amd64/aks-engine deploy \
+--azure-env AzureStackCloud \
+--location ${LOCATION} \
+--resource-group ${AKS_RESOURCE_GROUP} \
+--api-model ./kubernetes-azurestack.json \
+--output-directory ${AKS_RESOURCE_GROUP} \
+--client-id ${AZURE_CLIENT_ID} \
+--client-secret ${AZURE_CLIENT_SECRET} \
+--subscription-id ${AZURE_SUBSCRIPTION_ID} \
+--set \
+orchestratorProfile.orchestratorRelease=${AKS_ORCHESTRATOR_RELEASE},\
+customCloudProfile.portalURL=https://portal.${SUFFIX_STORAGE_ENDPOINT},\
+linuxProfile.ssh.publicKeys[0].keyData="${SSH_PUBLIC_KEY}",\
+masterProfile.dnsPrefix=${AKS_MASTER_DNS_PREFIX},\
+masterProfile.vmSize=${AKS_MASTER_VMSIZE},\
+masterProfile.count=${AKS_MASTER_COUNT},\
+masterProfile.distro=${AKS_DISTRO},\
+agentPoolProfiles[0].vmSize=${AKS_AGENT_VMSIZE},\
+agentPoolProfiles[0].count=${AKS_AGENT_COUNT},\
+agentPoolProfiles[0].distro=${AKS_DISTRO},\
+servicePrincipalProfile.clientId=${AZURE_CLIENT_ID},\
+servicePrincipalProfile.secret=${AZURE_CLIENT_SECRET} \
+--debug
+
+cp ${AKS_RESOURCE_GROUP}/apimodel.json /apimodel
+cp ${AKS_RESOURCE_GROUP}/kubeconfig/kubeconfig.local.json /kubeconfig
+tar -czvf installation/installation-${AKS_RESOURCE_GROUP}.tar.gz ./${AKS_RESOURCE_GROUP}
+
 ls -lisaR
-find / -name aks-engine -print
 printenv
