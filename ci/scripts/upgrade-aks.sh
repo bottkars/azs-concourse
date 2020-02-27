8b2517530ac63c38d3db4f6d80884701be91f613
Karsten Bott <karsten.bott@bottnet.de>
2020-02-27 10:08:10 +0100

    temp force

--- ci/scripts/upgrade-aks.sh
+++ ci/scripts/upgrade-aks.sh
@@ -16,45 +16,17 @@ set -eux
 az account set --subscription ${AZURE_SUBSCRIPTION_ID}
 TAG=$(cat aks-engine/tag)
 tar xzfv aks-engine/aks-engine-${TAG}-linux-amd64.tar.gz
-alias aks-engine=aks-engine-${TAG}-linux-amd64/aks-engine
-export SSL_CERT_FILE=${AZURE_CLI_CA_PATH}
-# ./aks-engine-${TAG}-linux-amd64/aks-engine deploy \
-#--azure-env AzureStackCloud \
-#--location ${LOCATION} \
-#--resource-group ${AKS_RESOURCE_GROUP} \
-#--api-model apimodel-json/kubernetes-azurestack.json \
-#--output-directory ${AKS_RESOURCE_GROUP} \
-#--client-id ${AZURE_CLIENT_ID} \
-#--client-secret ${AZURE_CLIENT_SECRET} \
-#--subscription-id ${AZURE_SUBSCRIPTION_ID} \
-#--set \
-#orchestratorProfile.orchestratorRelease=${AKS_ORCHESTRATOR_RELEASE},\
-#customCloudProfile.portalURL=https://portal.${SUFFIX_STORAGE_ENDPOINT},\
-#linuxProfile.ssh.publicKeys[0].keyData="${SSH_PUBLIC_KEY}",\
-#masterProfile.dnsPrefix=${AKS_MASTER_DNS_PREFIX},\
-#masterProfile.vmSize=${AKS_MASTER_VMSIZE},\
-#masterProfile.count=${AKS_MASTER_COUNT},\
-#masterProfile.distro=${AKS_MASTER_DISTRO},\
-#agentPoolProfiles[0].vmSize=${AKS_AGENT_VMSIZE},\
-#agentPoolProfiles[0].count=${AKS_AGENT_COUNT},\
-#agentPoolProfiles[0].distro=${AKS_AGENT_DISTRO},\
-#servicePrincipalProfile.clientId=${AZURE_CLIENT_ID},\
-#servicePrincipalProfile.secret=${AZURE_CLIENT_SECRET} \
-#--debug
-timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
-export timestamp
 
-
-
- aks-engine upgrade \
+aks-engine-${TAG}-linux-amd64/aks-engine upgrade \
     --api-model current-installation/${AKS_RESOURCE_GROUP}/apimodel.json \
     --resource-group $AKS_RESOURCE_GROUP --location ${LOCATION}\
     --upgrade-version ${AKS_ORCHESTRATOR_VERSION_UPDATE} --client-id ${AZURE_CLIENT_ID} \
     --client-secret ${AZURE_CLIENT_SECRET} \
     --subscription-id ${AZURE_SUBSCRIPTION_ID} \
-    --azure-env AzureStackCloud
+    --azure-env AzureStackCloud --force
 
-
+timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
+export timestamp
 # ls -lisaR /    
 APIMODEL_OUTPUT_FILE="$(echo "$APIMODEL_FILE" | envsubst '$timestamp')"
 cp current-installation/${AKS_RESOURCE_GROUP}/apimodel.json apimodel/"$APIMODEL_OUTPUT_FILE"
