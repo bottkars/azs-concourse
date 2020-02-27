07f704ad9ad97c391685dab8bd8ef15f91eec381
Karsten Bott <karsten.bott@bottnet.de>
2020-02-27 09:12:43 +0100

    installation upgrade and trigger

--- ci/scripts/upgrade-aks.sh
+++ ci/scripts/upgrade-aks.sh
@@ -16,6 +16,7 @@ set -eux
 az account set --subscription ${AZURE_SUBSCRIPTION_ID}
 TAG=$(cat aks-engine/tag)
 tar xzfv aks-engine/aks-engine-${TAG}-linux-amd64.tar.gz
+alias aks-engine=aks-engine-${TAG}-linux-amd64/aks-engine
 export SSL_CERT_FILE=${AZURE_CLI_CA_PATH}
 # ./aks-engine-${TAG}-linux-amd64/aks-engine deploy \
 #--azure-env AzureStackCloud \
@@ -44,7 +45,8 @@ timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
 export timestamp
 
 
- ./aks-engine-v0.43.3-linux-amd64/aks-engine upgrade \
+
+ aks-engine upgrade \
     --api-model current-installation/${AKS_RESOURCE_GROUP}/apimodel.json \
     --resource-group $AKS_RESOURCE_GROUP --location ${LOCATION}\
     --upgrade-version ${AKS_ORCHESTRATOR_VERSION_UPDATE} --client-id ${AZURE_CLIENT_ID} \
@@ -55,11 +57,11 @@ export timestamp
 
 # ls -lisaR /    
 APIMODEL_OUTPUT_FILE="$(echo "$APIMODEL_FILE" | envsubst '$timestamp')"
-cp ${AKS_RESOURCE_GROUP}/apimodel.json apimodel/"$APIMODEL_OUTPUT_FILE"
+cp current-installation/${AKS_RESOURCE_GROUP}/apimodel.json apimodel/"$APIMODEL_OUTPUT_FILE"
 
 KUBECONFIG_OUTPUT_FILE="$(echo "$KUBECONFIG_FILE" | envsubst '$timestamp')"
-cp ${AKS_RESOURCE_GROUP}/kubeconfig/kubeconfig.*.json kubeconfig/"${KUBECONFIG_OUTPUT_FILE}"
+cp current-installation/${AKS_RESOURCE_GROUP}/kubeconfig/kubeconfig.*.json kubeconfig/"${KUBECONFIG_OUTPUT_FILE}"
 
 INSTALLATION_OUTPUT_FILE="$(echo "$INSTALLATION_FILE" | envsubst '$timestamp')" 
-zip -r aks-installation/"${INSTALLATION_OUTPUT_FILE}" ${AKS_RESOURCE_GROUP}
+zip -r aks-installation/"${INSTALLATION_OUTPUT_FILE}" current-installation/${AKS_RESOURCE_GROUP}
 
