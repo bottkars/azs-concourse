bf4f1a96cfe7ecc7f1a2ade3b01272df2cb7a3b8
Karsten Bott <karsten.bott@bottnet.de>
2020-02-28 12:10:10 +0100

    latest

--- ci/scripts/query-aks-agent-pool.sh
+++ ci/scripts/query-aks-agent-pool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-echo "${CA_CERT}" >> ${AZURE_CLI_CA_PATH} # beware in "" for keep as single literal
+# echo "${CA_CERT}" >> ${AZURE_CLI_CA_PATH} # beware in "" for keep as single literal
 #az cloud register -n AzureStackUser \
 #--endpoint-resource-manager ${ENDPOINT_RESOURCE_MANAGER} \
 #--suffix-storage-endpoint ${SUFFIX_STORAGE_ENDPOINT} \
