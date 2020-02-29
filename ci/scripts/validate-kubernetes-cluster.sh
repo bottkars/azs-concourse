324111b2468a5d76840430f8625994c8aaeef0ad
Karsten Bott <karsten.bott@bottnet.de>
2020-02-29 18:38:44 +0100

    added sonobuoy validation tests

--- ci/scripts/validate-kubernetes-cluster.sh
+++ ci/scripts/validate-kubernetes-cluster.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -eu
 
-ls -lisaR /
+
+SONOBUOY_VERSION=$(cat sonobuoy-release/version)
+tar xzf sonobuoy-release/sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz
+echo "Starting Sonobuoy Validation"
+./sonobuoy run --kubeconfig kubeconfig/kubeconfig-*.json --wait
+RESULTS=$(./sonobuoy retrieve --kubeconfig kubeconfig/kubeconfig-*.json)
+./sonobuoy results ${RESULTS}
+cp ${RESULTS} validation-report/
+echo "Removing Sonobuoy"
+./sonobuoy delete --kubeconfig kubeconfig/kubeconfig-*.json --wait
 # echo $STATE | jq --arg iaas azurestack '. + {IAAS: $iaas}' > ${generated_state_path} 
 # ls -al
 # ls -al generated-state
