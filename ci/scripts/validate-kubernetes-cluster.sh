0da6ddfe62982ddcaf72ce89c77ca9410e46ccdc
Karsten Bott <karsten.bott@bottnet.de>
2020-03-01 07:53:08 +0100

    SONOBUOY_MODE

--- ci/scripts/validate-kubernetes-cluster.sh
+++ ci/scripts/validate-kubernetes-cluster.sh
@@ -5,7 +5,7 @@ set -euy
 SONOBUOY_VERSION=$(cat sonobuoy-release/version)
 tar xzf sonobuoy-release/sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz
 echo "Starting Sonobuoy Validation"
-./sonobuoy run --kubeconfig kubeconfig/kubeconfig-*.json --mode quick --wait
+./sonobuoy run --kubeconfig kubeconfig/kubeconfig-*.json --mode ${SONOBUOY_MODE} --wait
 RESULTS=$(./sonobuoy retrieve --kubeconfig kubeconfig/kubeconfig-*.json)
 ./sonobuoy results ${RESULTS}
 echo "$RESULTS"
@@ -13,7 +13,6 @@ cp ${RESULTS} validation-report/
 echo "Removing Sonobuoy"
 ./sonobuoy delete --kubeconfig kubeconfig/kubeconfig-*.json --wait
 
-exit 1
 
 # echo $STATE | jq --arg iaas azurestack '. + {IAAS: $iaas}' > ${generated_state_path} 
 # ls -al
