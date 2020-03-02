d8f9f63ffc3f29d2938f2b4134080748b1527bf5
Karsten Bott <karsten.bott@bottnet.de>
2020-03-02 07:16:04 +0100

    revert

--- ci/scripts/install-kubectl.sh
+++ ci/scripts/install-kubectl.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -eux
+set -eu
 # KUBECTL_VERSION=$(cat kubectl-release/version)
-# KUBECTL_VERSION=$(curl https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-# echo $KUBECTL_VERSION
+KUBECTL_VERSION=$(curl https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+echo $KUBECTL_VERSION
 
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/OS_DISTRIBUTION/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 
 export KUBECONFIG=kubeconfig/kubeconfig-$(cat kubeconfig/version).json
