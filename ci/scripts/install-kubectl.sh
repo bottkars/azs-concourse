6cdae7f5a6d1254b1aaca648aa103a6a22f19423
Karsten Bott <karsten.bott@bottnet.de>
2020-03-02 07:14:35 +0100

    single liner

--- ci/scripts/install-kubectl.sh
+++ ci/scripts/install-kubectl.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -eux
 # KUBECTL_VERSION=$(cat kubectl-release/version)
-KUBECTL_VERSION=$(curl https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-echo $KUBECTL_VERSION
+# KUBECTL_VERSION=$(curl https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+# echo $KUBECTL_VERSION
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+# curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/OS_DISTRIBUTION/amd64/kubectl
 chmod +x ./kubectl
 
 export KUBECONFIG=kubeconfig/kubeconfig-$(cat kubeconfig/version).json
 
 ./kubectl cluster-info
+
+./kubectl get nodes
 
 # echo $STATE | jq --arg iaas azurestack '. + {IAAS: $iaas}' > ${generated_state_path} 
 # ls -al
