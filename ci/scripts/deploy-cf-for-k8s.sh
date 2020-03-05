7cdadc07c58ad9be254f3f0010ec603e8c1d3051
Karsten Bott <karsten.bott@bottnet.de>
2020-03-05 21:01:16 +0100

    latest installer

--- ci/scripts/deploy-cf-for-k8s.sh
+++ ci/scripts/deploy-cf-for-k8s.sh
@@ -13,8 +13,8 @@ az login --service-principal \
     -p ${AZURE_CLIENT_SECRET} \
     --tenant ${AZURE_TENANT_ID}
 az account set --subscription ${AZURE_SUBSCRIPTION_ID}
-# TAG=$(cat bosh-cli-release/version)
-#cp bosh-cli-release/bosh-cli-${TAG}-linux-amd64 /usr/local/bin/bosh
+TAG=$(cat bosh-cli-release/version)
+cp bosh-cli-release/bosh-cli-${TAG}-linux-amd64 /usr/local/bin/bosh
 bosh --version
 # KUBECTL_VERSION=$(cat kubectl-release/version)
 KUBECTL_VERSION=$(curl https://storage.googleapis.com/kubernetes-release/release/stable.txt)
@@ -34,13 +34,10 @@ curl -L https://k14s.io/install.sh | bash
 
 cf-for-k8s-master/hack/generate-values.sh "${DNS_DOMAIN}" > cf-values/cf-values.yml
 echo "Installing CF..."
-# cf-for-k8s-master/bin/install-cf.sh cf-values/cf-values.yml
+cf-for-k8s-master/bin/install-cf.sh cf-values/cf-values.yml
 # " get cf_admin_password from cf-values/cf-values.yml "
 echo "${DNS_DOMAIN}" 
 echo "Configuring DNS..."
-
-
-
 
 timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
 export timestamp
@@ -48,5 +45,4 @@ export timestamp
 CF_CONFIG_OUTPUT_FILE="$(echo "$CF_CONFIG_FILE:" | envsubst '$timestamp')"
 cp cf-values/cf-values.yml cf-values/"$CF_CONFIG_OUTPUT_FILE"
 
-break
 
