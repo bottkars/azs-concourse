c48401db3812732609166a961c7e22aeb0f3b372
Karsten Bott <karsten.bott@bottnet.de>
2020-03-15 08:20:11 +0100

    added templates for aks networking

---
+++ ci/scripts/create-vnets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+DEPLOYMENT=$(\
+az deployment group create \
+--mode Incremental \
+--template-file ~/workspace/azs-concourse/templates/aks-network-azuredeploy.json \
+--resource-group AKSvnets \
+--parameters VNETName=AKSVnet0 \
+--parameters MasterSubnet=MasterSubnet0 \
+--parameters AgentSubnet=AgentSubnet0 \
+--parameters AgentAddressPrefix="10.200.0.0/24" \
+--parameters MasterAddressPrefix="10.100.0.0/24" \
+
+)
