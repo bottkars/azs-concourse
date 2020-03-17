296adfe9ac944f72ae946f7103f088a2f88633bc
bottkars <karsten.bott@bottnet.de>
2020-03-17 17:42:29 +0100

    update routes

--- ci/scripts/update-aks-routingtable.sh
+++ ci/scripts/update-aks-routingtable.sh
@@ -18,5 +18,3 @@ ROUTINGTABLE=$(az network route-table list -g ${RESOURCE_GROUP} -o json --query 
 az network vnet subnet update \
 --route-table ${ROUTINGTABLE} \
 --ids "/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${AKS_VNET_RG}/providers/Microsoft.Network/VirtualNetworks/${AKS_VNET_NAME}/subnets/${AKS_MASTER_SUBNET_NAME}"
-
-pause
