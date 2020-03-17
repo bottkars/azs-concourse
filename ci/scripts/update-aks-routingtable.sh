d2f1b52442d5b70d864995ab8db2febca3403a0b
bottkars <karsten.bott@bottnet.de>
2020-03-17 16:57:04 +0100

    added routingtable

---
+++ ci/scripts/update-aks-routingtable.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eux
+echo "${CA_CERT}" >> ${AZURE_CLI_CA_PATH} # beware in "" for keep as single literal
+az cloud register -n AzureStackUser \
+    --endpoint-resource-manager ${ENDPOINT_RESOURCE_MANAGER} \
+    --suffix-storage-endpoint ${SUFFIX_STORAGE_ENDPOINT} \
+    --suffix-keyvault-dns ${VAULT_DNS} \
+    --profile ${PROFILE}
+az cloud set -n AzureStackUser
+az cloud list --output table
+az login --service-principal \
+    -u ${AZURE_CLIENT_ID} \
+    -p ${AZURE_CLIENT_SECRET} \
+    --tenant ${AZURE_TENANT_ID}
+az account set --subscription ${AZURE_SUBSCRIPTION_ID}
+
+ROUTINGTABLE=$(az network route-table list -g ${RESOURCE_GROUP} -o json | jq -r '.[].id')
+az network vnet subnet update \
+-g RESOURCE_GROUP_NAME_VNET \
+--route-table ${ROUTINGTABLE} \
+--ids "/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${AKS_VNET_RG}/providers/Microsoft.Network/VirtualNetworks/${AKS_VNET_NAME}/subnets/${AKS_MASTER_SUBNET_NAME}"
+
+pause
