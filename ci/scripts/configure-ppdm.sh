2efd8d55353f312e3e508c07a68dd4694aeeb8bd
bottkars <karsten.bott@bottnet.de>
2020-05-12 12:45:22 +0200

    typo from lame text edit

--- ci/scripts/configure-ppdm.sh
+++ ci/scripts/configure-ppdm.sh
@@ -34,19 +34,18 @@ CONFIGURATION=$(curl -k -sS \
 NODE_ID=$(echo $CONFIGURATION | jq -r .nodeId)  
 CONFIGURATION_ID=$(echo $CONFIGURATION | jq -r .id)
 echo "retrieving session cookies"
-curl https://${PPDM_FQDN}:443  --cookie-jar cookies.txt -sk
+curl "https://${PPDM_FQDN}:443" --output /dev/null  --cookie-jar cookies.txt -sk
 XSRF_TOKEN=$(cat cookies.txt | grep "XSRF-TOKEN" | awk '{printf $7}')
 CSRF_COOKIE=$(cat cookies.txt | grep "_csrf" | awk '{printf $7}')
 # this is a dirty hack, going to build appliance config from jq merge
 echo "Posting appliance Configuration using CSRF Cookies"
-curl -k -b cookies.txt --request PUT \
+curl -k -bs cookies.txt --request PUT \
   --url "https://${PPDM_FQDN}:8443/api/v2/configurations/${CONFIGURATION_ID}" \
   --header "content-type: application/json" \
   --header "XSRF-TOKEN: TTxWzEFj-mb_RmpGa7rAF8IYjjI08mASfLrw" \
   --header "_csrf: 2lc9OmrvvspVHIordtkLr_2i" \
   --header "Authorization: Bearer ${TOKEN}" \
-  --data '
-{
+  --data '{
     "id": "'${CONFIGURATION_ID}'",
     "nodeId": "'${NODE_ID}'",
     "networks": [
@@ -91,34 +90,34 @@ curl -k -b cookies.txt --request PUT \
         "description": "OS root user account",
         "numberOfDaysToExpire": 59,
         "password": "changeme",
-        "newPassword": ":'${PPDM_PASSWORD}'"
+        "newPassword": "'${PPDM_PASSWORD}'"
       },
       {
         "userName": "admin",
         "description": "OS administrator user account",
         "numberOfDaysToExpire": 59,
         "password": "@ppAdm1n",
-        "newPassword": ":'${PPDM_PASSWORD}'"
+        "newPassword": "'${PPDM_PASSWORD}'"
       },
       {
         "userName": "support",
         "description": "OS support user account",
         "numberOfDaysToExpire": 59,
         "password": "$upp0rt!",
-        "newPassword": ":'${PPDM_PASSWORD}'"
+        "newPassword": "'${PPDM_PASSWORD}'"
       }
     ],
     "lockbox": {
       "name": "Lockbox",
       "lastUpdatedTime": "2020-05-11T18:37:02.576+0000",
       "passphrase": "Ch@ngeme1",
-      "newPassphrase": ":'${PPDM_PASSWORD}'"
+      "newPassphrase": "'${PPDM_PASSWORD}'"
     },
     "configType": "standalone",
     "gettingStartedCompleted": false,
     "autoSupport": false,
     "integratedStorageSecuritySetupCompleted": false,
-    "applicationUserPassword": ":'${PPDM_PASSWORD}'"
+    "applicationUserPassword": "'${PPDM_PASSWORD}'"
   }'
 
 
