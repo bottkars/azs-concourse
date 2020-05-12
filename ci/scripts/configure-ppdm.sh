5ab776a03d077f35bd7d5a7cdaa3961bdfd709a6
bottkars <karsten.bott@bottnet.de>
2020-05-12 10:40:07 +0200

    initial ppdm task

--- ci/scripts/configure-ppdm.sh
+++ ci/scripts/configure-ppdm.sh
@@ -4,143 +4,110 @@ set -eux
 echo "installing jq...."
 DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 
-until $(curl --output /dev/null --silent --head  -k -X GET "https://${PPDM_FQDN}:8443/api/v2/info"); do
-    printf '.'
-    sleep 5
-done
-
-curl --request POST \
-  --url "https://${PPDM_FQDN}:8443/api/v2/login" -k \
-  --header 'content-type: application/json' \
-  --data '{"username":"admin","password":"admin"}'
+### get api token
+echo "requesting API token"
 
 TOKEN=$(curl -s --request POST \
   --url "https://${PPDM_FQDN}:8443/api/v2/login" -k \
   --header 'content-type: application/json' \
   --data '{"username":"admin","password":"admin"}' | jq -r .access_token)
+echo "retrieving initial appliance configuration"
 
-CONFIGURATION_ID=$(curl -k -sS \
+CONFIGURATION=$(curl -k -sS \
   --header "Authorization: Bearer ${TOKEN}" \
   --fail \
-  --url "https://${PPDM_FQDN}:8443/api/v2/configurations" | jq -r ".content[0].id")
+  --url "https://${PPDM_FQDN}:8443/api/v2/configurations" | jq -r ".content[0]")
 
-curl -k -vL --request PUT \
-  --url "https://${PPDM_FQDN}/api/v2/configurations/${CONFIGURATION_ID}" \
-  --header "authorization: Bearer ${TOKEN}" \
+NODE_ID=$(echo $CONFIGURATION | jq -r .nodeId)  
+CONFIGURATION_ID=$(echo $CONFIGURATION | jq -r .id)
+echo "retrieving session cookies"
+curl https://${PPDM_FQDN}:443  --cookie-jar cookies.txt -sk
+XSRF_TOKEN=$(cat cookies.txt | grep "XSRF-TOKEN" | awk '{printf $7}')
+CSRF_COOKIE=$(cat cookies.txt | grep "_csrf" | awk '{printf $7}')
+# this is a dirty hack, going to build appliance config from jq merge
+curl -k -b cookies.txt --verbose --request PUT \
+  --url "https://${PPDM_FQDN}:8443/api/v2/configurations/${CONFIGURATION_ID}" \
   --header "content-type: application/json" \
+  --header "XSRF-TOKEN: TTxWzEFj-mb_RmpGa7rAF8IYjjI08mASfLrw" \
+  --header "_csrf: 2lc9OmrvvspVHIordtkLr_2i" \
+  --header "Authorization: Bearer ${TOKEN}" \
   --data '
 {
-    "applicationUserPassword": "Password123!",
-    "autoSupport": true,
-    "configType": "standalone",
-    "lockbox": {
-        "newPassphrase": "Password123!",
-        "passphrase": "Password123!"            
-    
-    },
+    "id": "'${CONFIGURATION_ID}'",
+    "nodeId": "'${NODE_ID}'",
     "networks": [
-        {
-            "dnsServers": [
-                "192.168.1.44",
-                "192.168.1.1",
-                "8.8.8.8"
-            ],
-            "fqdn": "pp-dr.home.labbuildr.com",
-            "gateway": "100.250.1.1",
-            "interfaceName": "eth0",
-            "ipAddress": [
-                "100.250.1.130"
-            ],
-            "ipAddressFamily": "IPv4",
-            "netMask": "255.255.255.0",
-            "nslookupSuccess": true,
-        }
+      {
+        "fqdn": "'${PPDM_FQDN}'",
+        "ipAddress": [
+          "'${PPDM_ADDRESS}'"
+        ],
+        "ipAddressFamily": "IPv4",
+        "interfaceName": "eth0",
+        "netMask": "'${PPDM_NETMASK}'",
+        "gateway": "'${PPDM_GATEWAY}'",
+        "dnsServers": [
+          "'${PPDM_DNS}'"
+        ],
+        "nslookupSuccess": false
+      },
+      {
+        "ipAddress": [
+          "172.24.0.1"
+        ],
+        "ipAddressFamily": "IPv4",
+        "interfaceName": "brpp0",
+        "netMask": "255.255.255.0"
+      },
+      {
+        "ipAddress": [
+          "172.17.0.1"
+        ],
+        "ipAddressFamily": "IPv4",
+        "interfaceName": "docker0",
+        "netMask": "255.255.0.0"
+      }
     ],
     "ntpServers": [
-        "192.168.1.1"
+      "192.168.1.1"
     ],
+    "timeZone": "Europe/Berlin - Central European Time",
     "osUsers": [
-        {
-            "userName": "root",
-            "password": "Password123!",            
-            "newPassword": "Password123!",
-            "description": "OS root user account",
-            "numberOfDaysToExpire": 60
-        },
-        {
-            "userName": "admin",
-            "password": "Password123!",            
-            "newPassword": "Password123!",
-            "numberOfDaysToExpire": 60
-        },
-        {
-            "userName": "support",
-            "password": "Password123!",            
-            "newPassword": "Password123!",
-            "numberOfDaysToExpire": 60
-        }
+      {
+        "userName": "root",
+        "description": "OS root user account",
+        "numberOfDaysToExpire": 59,
+        "password": "changeme",
+        "newPassword": ":'${PPDM_PASSWORD}'"
+      },
+      {
+        "userName": "admin",
+        "description": "OS administrator user account",
+        "numberOfDaysToExpire": 59,
+        "password": "@ppAdm1n",
+        "newPassword": ":'${PPDM_PASSWORD}'"
+      },
+      {
+        "userName": "support",
+        "description": "OS support user account",
+        "numberOfDaysToExpire": 59,
+        "password": "$upp0rt!",
+        "newPassword": ":'${PPDM_PASSWORD}'"
+      }
     ],
-    "timeZone": " Europe/Berlin - Central European Time"
-}'
+    "lockbox": {
+      "name": "Lockbox",
+      "lastUpdatedTime": "2020-05-11T18:37:02.576+0000",
+      "passphrase": "Ch@ngeme1",
+      "newPassphrase": ":'${PPDM_PASSWORD}'"
+    },
+    "configType": "standalone",
+    "gettingStartedCompleted": false,
+    "autoSupport": false,
+    "integratedStorageSecuritySetupCompleted": false,
+    "applicationUserPassword": ":'${PPDM_PASSWORD}'"
+  }'
 
-curl -k -sS \
+curl -k  \
   --header "Authorization: Bearer ${TOKEN}" \
   --fail \
   --url "https://${PPDM_FQDN}:8443/api/v2/configurations/${CONFIGURATION_ID}/config-status"
-
-/api/v2/configurations/{configurationId}/config-status
-
-
-
-#####
-
-
-"id" : "7aabdac9-b354-4646-a36f-dcd0641fe19f",
-    "activationDate" : "2020-05-12T04:48:11.710Z",
-    "type" : "TRIAL",
-    "status" : "VALID",
-    "key" : "Trial Key",
-    "licenseKeys" : [ {
-      "featureName" : "POWERPROTECT SW TRIAL",
-      "startDate" : "2020-05-12T04:48:11.710Z",
-      "endDate" : "2020-08-10T04:48:11.710Z",
-      "gracePeriod" : "2020-08-10T04:48:11.710Z",
-      "plc" : "PPDM"
-
-curl -k -sS \
-  --header "Authorization: Bearer ${TOKEN}" \
-  --fail \
-  --url "https://${PPDM_FQDN}:8443/api/v2/common-settings"
-
-
-/api/v2/common-settings
-
-
-
-
-####
-
-curl -k -vX --request PUT \
-  --url https://${PPDM_FQDN}/api/v2/configurations/5a1bdf39-83c8-4cf4-a782-1763cf78f152 \
-  --header "Authorization: Bearer ${TOKEN}" \
-  --header "content-type: application/json" \
-  --data '
-{
-"osUsers": [{
-"userName": "root",
-"newPassword": "Password123!",
-"numberOfDaysToExpire": 60
-},
-{
-"userName": "admin",
-"newPassword": "Password123!",
-"numberOfDaysToExpire": 60
-},
-{
-"userName": "support",
-"newPassword": "Password123!",
-"numberOfDaysToExpire": 60
-}
-],
-"timeZone": "PST8PDT - Pacific Standard Time"
-}'
