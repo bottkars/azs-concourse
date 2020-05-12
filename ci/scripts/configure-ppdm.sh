fdaa91041fed6db5d4a058e059db6a684f549f08
bottkars <karsten.bott@bottnet.de>
2020-05-12 21:03:46 +0200

    demo 0 iteratio

--- ci/scripts/configure-ppdm.sh
+++ ci/scripts/configure-ppdm.sh
@@ -21,8 +21,8 @@ TOKEN=$(curl -k -sS --request POST \
 echo "Retrieving initial appliance configuration Template"
 curl -k -sS --request GET \
     --header "Authorization: Bearer ${TOKEN}" \
-    --url "https://${PPDM_FQDN}:8443/api/v2/configurations" | jq -r ".content[0]"
-    
+    --url "https://${PPDM_FQDN}:8443/api/v2/configurations"
+
 CONFIGURATION=$(curl -k -sS --request GET \
     --header "Authorization: Bearer ${TOKEN}" \
     --url "https://${PPDM_FQDN}:8443/api/v2/configurations" | jq -r ".content[0]" )
