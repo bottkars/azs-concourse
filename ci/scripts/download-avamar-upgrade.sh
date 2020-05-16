f9112a9f8b28c9656e19e32b99d3562b95816480
bottkars <karsten.bott@bottnet.de>
2020-05-16 07:18:38 +0200

    latest

--- ci/scripts/download-avamar-upgrade.sh
+++ ci/scripts/download-avamar-upgrade.sh
@@ -3,5 +3,16 @@ set -eu
 
 echo "installing jq...."
 DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
-exit 1
-break some stuff
+AV_BASEURL=$(echo $avamar | jq -r ".BaseURL")
+AV_LOCATION=$(echo $avamar | jq -r ".location")
+AV_RELEASE=$(echo $avamar | jq -r ".release")
+AV_VERSION=$(echo $avamar | jq -r ".version")
+
+# for PACKAGE in AvamarUpgrade UpgradeAvinstaller UpgradeClientDownloads UpgradeClientPluginCatalog ChangeNetworkSettings
+
+for PACKAGE in UpgradeAvinstaller  ChangeNetworkSettings
+do
+    echo "downloading ${PACKAGE} ${AV_RELEASE}"
+    curl -v --disable -epsv -o ${PACKAGE}/${PACKAGE}-${AV_RELEASE}.avp ${AV_BASEURL}/${AV_LOCATION}/${PACKAGE}-${AV_RELEASE}.avp
+done
+
