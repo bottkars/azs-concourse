a4363f38f700bfccc3112223de029b7e926e29b4
bottkars <karsten.bott@bottnet.de>
2020-05-16 09:32:48 +0200

    lokation and packages

--- ci/scripts/download-avamar-packages.sh
+++ ci/scripts/download-avamar-packages.sh
@@ -4,7 +4,7 @@ set -eu
 echo "installing jq...."
 DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 AV_BASEURL=$(echo $avamar | jq -r ".BaseURL")
-AV_LOCATION=$(echo $avamar | jq -r ".location")
+# AV_LOCATION=$(echo $avamar | jq -r ".location")
 AV_RELEASE=$(echo $avamar | jq -r ".release")
 AV_VERSION=$(echo $avamar | jq -r ".version")
 
