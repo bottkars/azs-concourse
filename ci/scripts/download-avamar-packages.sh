cbdb8c41ff70d95f20019779362d5140dfd4d556
bottkars <karsten.bott@bottnet.de>
2020-05-16 09:43:41 +0200

    av release

--- ci/scripts/download-avamar-packages.sh
+++ ci/scripts/download-avamar-packages.sh
@@ -5,10 +5,10 @@ echo "installing jq...."
 DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 AV_BASEURL=$(echo $avamar | jq -r ".BaseURL")
 # AV_LOCATION=$(echo $avamar | jq -r ".location")
-AV_RELEASE=$(echo $avamar | jq -r ".release")
+# AV_RELEASE=$(echo $avamar | jq -r ".release")
 AV_VERSION=$(echo $avamar | jq -r ".version")
 
-
+# ftp://avamar_ftp:anonymous@ftp.emc.com/software/av3494954704/AVE-19.2.0.155.ova
 for PACKAGE in $packages
 do
     echo "downloading ${PACKAGE} ${AV_RELEASE}"
