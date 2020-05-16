710c7d30fca1f03b2c6f6c797148388e8660dad9
bottkars <karsten.bott@bottnet.de>
2020-05-16 07:44:50 +0200

    latest

--- ci/scripts/download-avamar-upgrade.sh
+++ ci/scripts/download-avamar-upgrade.sh
@@ -8,9 +8,8 @@ AV_LOCATION=$(echo $avamar | jq -r ".location")
 AV_RELEASE=$(echo $avamar | jq -r ".release")
 AV_VERSION=$(echo $avamar | jq -r ".version")
 
-# for PACKAGE in AvamarUpgrade UpgradeAvinstaller UpgradeClientDownloads UpgradeClientPluginCatalog ChangeNetworkSettings
-
-for PACKAGE in UpgradeAvinstaller  ChangeNetworkSettings
+for PACKAGE in AvamarUpgrade UpgradeAvinstaller UpgradeClientDownloads UpgradeClientPluginCatalog ChangeNetworkSettings
+# for PACKAGE in UpgradeAvinstaller  ChangeNetworkSettings
 do
     echo "downloading ${PACKAGE} ${AV_RELEASE}"
     echo "from ${AV_BASEURL}/${AV_LOCATION}/${PACKAGE}-${AV_RELEASE}.avp"
