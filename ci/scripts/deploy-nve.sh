4090b2f16c2f44c5e1d80ac00a2757a6cf1d7a05
bottkars <karsten.bott@bottnet.de>
2020-05-18 08:28:25 +0200

    nve setup

--- ci/scripts/deploy-nve.sh
+++ ci/scripts/deploy-nve.sh
@@ -9,14 +9,21 @@ echo "installing jq...."
 DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
 govc import.spec networker/NVE-${NVE_VERSION}.ova > networker.json
 echo "configuring appliance (vami) settings"
-exit1
-break
-jq  --arg address "${NVE_ADDRESS}" '(.PropertyMapping[] | select(.Key == "vami.ip0.brs") | .Value) |= $address' networker.json > "tmp" && mv "tmp" networker.json
-jq  --arg gateway "${NVE_GATEWAY}" '(.PropertyMapping[] | select(.Key == "vami.gateway.brs") | .Value) |= $gateway' networker.json > "tmp" && mv "tmp" networker.json
-jq  --arg netmask "${NVE_NETMASK}" '(.PropertyMapping[] | select(.Key == "vami.netmask0.brs") | .Value) |= $netmask' networker.json  > "tmp" && mv "tmp" networker.json
-jq  --arg dns "${NVE_DNS}" '(.PropertyMapping[] | select(.Key == "vami.DNS.brs") | .Value) |= $dns' networker.json  > "tmp" && mv "tmp" networker.json
-jq  --arg fqdn "${NVE_FQDN}" '(.PropertyMapping[] | select(.Key == "vami.fqdn.brs") | .Value) |= $fqdn' networker.json  > "tmp" && mv "tmp" networker.json
-jq  --arg network "${NVE_NETWORK}" '(.NetworkMapping[].Name |= $network)' networker.json  > "tmp" && mv "tmp" networker.json
+
+jq  '(.PropertyMapping[] | select(.Key == "vami.ipv4.NetWorker_Virtual_Edition") | .Value) |= env.NVE_ADDRESS' networker.json > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.gatewayv4.NetWorker_Virtual_Edition") | .Value) |= env.NVE_GATEWAY' networker.json > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.DNS.NetWorker_Virtual_Edition") | .Value) |= env.NVE_DNS' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.searchpaths.NetWorker_Virtual_Edition") | .Value) |= env.NVE_SEARCHPATHS' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.NVEtimezone.NetWorker_Virtual_Edition") | .Value) |= env.NVE_TIMEZONE' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.NTP.NetWorker_Virtual_Edition") | .Value) |= env.NVE_NTP' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.vCenterFQDN.NetWorker_Virtual_Edition") | .Value) |= env.GOVC_URL' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.vCenterUsername.NetWorker_Virtual_Edition") | .Value) |= env.GOVC_USERNAME' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.PropertyMapping[] | select(.Key == "vami.FQDN.NetWorker_Virtual_Edition") | .Value) |= env.NVE_FQDN' networker.json  > "tmp" && mv "tmp" networker.json
+# jq  '(.NetworkMapping[].Name |= env.NVE_NETWORK)' networker.json  > "tmp" && mv "tmp" networker.json
+# jq  '(.NetworkMapping[].Network |= "ethernet-0")' networker.json  > "tmp" && mv "tmp" networker.json
+# jq  '(.PowerOn |= false)' networker.json  > "tmp" && mv "tmp" networker.json
+jq  '(.InjectOvfEnv |= true)' networker.json  > "tmp" && mv "tmp" networker.json
+
 echo "importing networker ${NVE_VERSION} NVE template"
 govc import.ova -name ${NVE_VMNAME}  -options=networker.json networker/NVE-${NVE_VERSION}.ova
 govc vm.network.change -vm ${NVE_VMNAME} -net=VLAN250 ethernet-0
