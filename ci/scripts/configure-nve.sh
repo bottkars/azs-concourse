851904dcd31e471f980aa6ef7f0bfef7bc95684d
bottkars <karsten.bott@bottnet.de>
2020-05-18 14:18:05 +0200

    latest

--- ci/scripts/configure-nve.sh
+++ ci/scripts/configure-nve.sh
@@ -2,12 +2,14 @@
 set -eu
 
 echo "installing jq...."
-DEBIAN_FRONTEND=noninteractive apt-get install -qq jq < /dev/null > /dev/null
-
+# DEBIAN_FRONTEND=noninteractive apt-get install -qq jq sshpass < /dev/null > /dev/null
 exit1
 
-timezone_name: "Europe/Berlin"
-admin_password_os: "Password123!"
+sshpass -p "changeme" /usr/bin/ssh -o "StrictHostKeyChecking no"  \
+admin@nve-dr.home.labbuildr.com \
+avi-cli --user root --password "changeme" --install localhost \
+timezone_name=${NVE_TIMEZONE}
+admin_password_os=${NVE_ADMIN_PASSWORD_OS}
 root_password_os: "Password123!"
 snmp_string: public
 datadomain_host: ddve1.home.labbuildr.com
