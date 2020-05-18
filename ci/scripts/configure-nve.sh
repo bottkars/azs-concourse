27405833ac03a4c24cafeb690db260f433e3a673
bottkars <karsten.bott@bottnet.de>
2020-05-18 14:22:19 +0200

    ddboost user

--- ci/scripts/configure-nve.sh
+++ ci/scripts/configure-nve.sh
@@ -8,16 +8,16 @@ exit1
 sshpass -p "changeme" /usr/bin/ssh -o "StrictHostKeyChecking no"  \
 admin@nve-dr.home.labbuildr.com \
 avi-cli --user root --password "changeme" --install localhost \
-timezone_name=${NVE_TIMEZONE}
-admin_password_os=${NVE_ADMIN_PASSWORD_OS}
-root_password_os: "Password123!"
-snmp_string: public
-datadomain_host: ddve1.home.labbuildr.com
-storage_path: nvedr
-ddboost_user: boostnvedr
-ddboost_user_pwd: "Password123!"
-ddboost_user_pwd_cf: "Password123!"
-datadomain_sysadmin: sysadmin
-datadomain_sysadmin_pwd: "Password123!"
-tomcat_keystore_password: "Password123!"
-authc_admin_password: "Password123!"
+timezone_name=${NVE_TIMEZONE} \
+admin_password_os=${NVE_ADMIN_PASSWORD_OS} \
+root_password_os=${NVE_ROOT_PASSWORD_OS} \
+snmp_string=${NVE_SNMP_STRING} \
+datadomain_host=${NVE_DATADOMAIN_HOST} \
+storage_path=${NVE_STORAGE_PATH} \
+ddboost_user=${} \
+ddboost_user_pwd=${} \
+ddboost_user_pwd_cf=${} \
+datadomain_sysadmin=${} \
+datadomain_sysadmin_pwd=${} \
+tomcat_keystore_password=${} \
+authc_admin_password=${} 
