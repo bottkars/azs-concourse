d38f70d007498b95960f57970d14fc2cc53f885e
“bottkars” <Karsten.bott@bottnet.de>
2020-09-01 05:40:19 +0200

    added basic validate arm template

---
+++ ci/scripts/validate-arm-template.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+echo "${CA_CERT}" >> ${AZURE_CLI_CA_PATH}
+az cloud register -n AzureStackUser \
+  --endpoint-resource-manager ${ENDPOINT_RESOURCE_MANAGER} \
+  --suffix-storage-endpoint ${SUFFIX_STORAGE_ENDPOINT} \
+  --suffix-keyvault-dns ${VAULT_DNS} \
+  --profile ${PROFILE}
+az cloud set -n AzureStackUser
+az login --service-principal \
+    -u ${AZURE_CLIENT_ID} \
+    -p ${AZURE_CLIENT_SECRET} \
+    --tenant ${AZURE_TENANT_ID}
+set -eux 
+az account set --subscription ${AZURE_SUBSCRIPTION_ID}
+az group create --name ${RESOURCE_GROUP} --location ${LOCATION}
+az group deployment validate \
+  --mode incremental \
+  --resource-group ${RESOURCE_GROUP} \
+  --parameters config/${PARAMETER_FILE} \
+  --parameters ${ADDITIONAL_PARAMETERS} \
+  --template-uri ${TEMPLATE_URI} \
+  --parameter location=${LOCATION}
+
+
+
