34dc448170f933de4c72700081f84dbcb6b3cfe2
“bottkars” <Karsten.bott@bottnet.de>
2020-09-01 07:06:57 +0200

    latests 1

--- ci/scripts/validate-template.sh
+++ ci/scripts/validate-template.sh
@@ -13,21 +13,15 @@ az login --service-principal \
     --tenant ${AZURE_TENANT_ID}
 set -eux 
 az account set --subscription ${AZURE_SUBSCRIPTION_ID}
-SOURCE_URI=$(grep -A0 "${OPSMAN_IMAGE_LOCATION}:" image/*.yml  | tail -n1 | awk '{ print $2 }')
-OPSMAN_VHD=$(basename $SOURCE_URI)
-IMAGE_URI=https://${ACCOUNT_NAME}.blob.${SUFFIX_STORAGE_ENDPOINT}/${DESTINATION_CONTAINER}/${OPSMAN_VHD}
-# opsManVHD will be overwritten to the actual source
 az group create --name ${RESOURCE_GROUP} --location ${LOCATION}
 az deployment group validate \
   --mode incremental \
   --resource-group ${RESOURCE_GROUP} \
-  --parameters config/${PARAMETER_FILE} \
+  --parameters ${PARAMETER_FILE} \
   --parameters ${ADDITIONAL_PARAMETERS} \
-  --parameters opsManVHD=${OPSMAN_VHD} \
-  --parameters OpsManImageURI=${IMAGE_URI} \
-  --parameters boshstorageaccountname=${BOSHSTORAGEACCOUNT} \
   --template-uri ${TEMPLATE_URI} \
-  --parameter location=${LOCATION}
+  --parameter location=${LOCATION} \
+  --debug
 
 
 
