b2dcefde1e30191568b558013f64d305a790f4bf
“bottkars” <Karsten.bott@bottnet.de>
2020-09-01 07:30:53 +0200

    added deployment

---
+++ ci/scripts/deploy-template.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+echo "${CA_CERT}" >> ${AZURE_CLI_CA_PATH}
+az cloud register -n AzureStackUser \
+  --endpoint-resource-manager ${ENDPOINT_RESOURCE_MANAGER} \
+  --suffix-storage-endpoint ${SUFFIX_STORAGE_ENDPOINT} \
+  --suffix-keyvault-dns ${VAULT_DNS} \
+  --profile ${PROFILE}
+az cloud set -n AzureStackUser
+az login --service-principal \
+    -u ${AZURE_CLIENT_ID} \
+    -p ${AZURE_CLIENT_SECRET} \
+    --tenant ${AZURE_TENANT_ID}
+set -eux 
+az account set --subscription ${AZURE_SUBSCRIPTION_ID}
+az group create --name ${RESOURCE_GROUP} --location ${LOCATION}
+az deployment group create \
+  --mode incremental \
+  --resource-group ${RESOURCE_GROUP} \
+  --parameters ${PARAMETER_FILE} \
+  --parameters ${ADDITIONAL_PARAMETERS} \
+  --template-uri ${TEMPLATE_URI} \
+  --query ${QUERY} #\
+#    >> deployment.json
+
+
+
